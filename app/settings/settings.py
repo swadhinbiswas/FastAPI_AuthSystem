f639699681c0026b3a7fec59ff94e63851998894
Swadhin Biswas <biswas15-6259@s.diu.edu.bd>
2024-08-01 08:02:07 +0000

    update

---
+++ app/settings/settings.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from decouple import config
+config.encoding('cp1251')
+
+
+class Settings:
+    DATABASENAE:Optional[str]=config('DATABASENAME')
+    DATABASEURL:Optional[str]= config('MONGODBURL')
+    DATABASEURL:Optional[str]=config('DATABASEURL')
+    DATABASEUSER:OPtional[str]=config('DATABSEUSER')
+    
+
+
+
